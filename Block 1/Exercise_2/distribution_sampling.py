2f868ec766f6131d4a45ebf2d8106cebca69ad73
nunziatodamino <nunziatodamino@gmail.com>
2024-11-08 15:37:37 +0100

    - Added .tex report to the repository - automatic saving in folder for images of exercise 2 - improved workflow latex + vscode

--- Block 1/Exercise_2/distribution_sampling.py
+++ Block 1/Exercise_2/distribution_sampling.py
@@ -10,12 +10,12 @@ def inversion(cdf_inverse: Callable[[float], float], number_sampled_points: int)
 
 def rejection_from_normal(target_distribution, c, mean, sigma, number_sampled_points):
     counter = 0
-    random_variables = np.zeros(number_sampled_points)
+    accepted_variables = np.zeros(number_sampled_points)
     while counter < number_sampled_points:
         candidate_random_variable = np.random.normal(mean, sigma)
         uniform_random_num = np.random.random()
         acceptance_threshold = target_distribution(candidate_random_variable)/ (c * stats.norm.pdf(candidate_random_variable, mean, sigma))
         if uniform_random_num <= acceptance_threshold:
-            random_variables[counter] = candidate_random_variable
+            accepted_variables[counter] = candidate_random_variable
             counter+=1
-    return random_variables      
+    return accepted_variables     
--- Block 1/Exercise_2/exercise_2.py
+++ Block 1/Exercise_2/exercise_2.py
@@ -1,7 +1,14 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import kv
+import scipy.stats as stats
 import distribution_sampling as sampling
+
+########################################
+report_path = "/home/nunziato-damino/Documents/Github/NMSM_2024/Block 1/Report"
+exercise_folder = "FIG/exercise_2_images"
+########################################
 
 A = np.sqrt(2)/(np.e * kv(1/4,1))
 
@@ -12,18 +19,35 @@ mean = 0
 sigma = np.sqrt(1/4)
 c = 1.6
 
+sample_space = np.arange(-3 ,+3, 0.01)
+target_density = non_invertible_pdf(sample_space)
+
+plt.rcParams.update({'font.size': 18})
+
+plt.figure(figsize=(10, 6))
+plt.plot (sample_space, target_density, label = "Target distribution")
+plt.plot(sample_space, c *stats.norm.pdf(sample_space, mean, sigma), label = "Candidate density times constant c")
+plt.xlabel("Random variable")
+plt.ylabel("Probability density")
+plt.legend(fontsize = 10)
+image_name = "rejection_sampling_inequality_check.png"
+entire_path = os.path.join(report_path, exercise_folder, image_name)
+plt.savefig(entire_path)
+plt.show()
+
 NUMBER_SAMPLED_VARIABLES = 10000
 
 random_variables = sampling.rejection_from_normal(non_invertible_pdf, c, mean, sigma, NUMBER_SAMPLED_VARIABLES)
 
-x = np.arange(-3 ,+3, 0.01)
-y = non_invertible_pdf(x)
-
 plt.rcParams.update({'font.size': 18})
 
-plt.plot (x,y, label = "Target distribution")
+plt.figure(figsize=(10, 6)) # essential to avoid overcropping in the save process
+plt.plot (sample_space, target_density, label = "Target distribution")
 plt.hist(random_variables, bins=30, density=True, alpha=0.4, color='r', label= "Sampled variables")
 plt.xlabel("Random variable")
 plt.ylabel("Probability density")
-plt.legend()
+plt.legend(fontsize = 10)
+image_name = "rejection_sampling_varify.png"
+entire_path = os.path.join(report_path, exercise_folder, image_name)
+plt.savefig(entire_path)
 plt.show()
