ab80303c8fc9ab1a0f4d2af67f6f6103f61ebd4f
nunziatodamino <nunziatodamino@gmail.com>
2024-11-22 16:10:36 +0100

    - completed revision of exercises 2, 3

--- Block 1/Exercise_3/importance_sampling.py
+++ Block 1/Exercise_3/importance_sampling.py
@@ -2,6 +2,10 @@ import numpy as np
 import scipy.stats as stats
 
 def crude_monte_carlo_uniform (function, left_bound, right_bound, iterations):
+    '''
+    Evaluates the integral of a function contained in the array function from the left_bound to the right_bound 
+    sampling from the uniform distribution between these bounds
+    '''
     values = np.random.uniform(left_bound, right_bound, size = iterations)
     func_values = function(values)
     estimate = (right_bound - left_bound ) * np.sum(func_values) / iterations
@@ -9,6 +13,10 @@ def crude_monte_carlo_uniform (function, left_bound, right_bound, iterations):
     return estimate, np.sqrt( variance / iterations ) 
 
 def crude_monte_carlo_normal (function, mean, sigma, iterations):
+    '''
+    Evaluates the integral of a function contained in the array function from the left_bound to the right_bound 
+    sampling from the normal distribution between these bounds
+    '''
     values = np.random.normal(mean, sigma, size = iterations)
     func_values = function(values)
     estimate = np.sum(func_values) / iterations
@@ -16,9 +24,18 @@ def crude_monte_carlo_normal (function, mean, sigma, iterations):
     return estimate, np.sqrt(variance / iterations)
 
 def truncated_normal (x ,mean, sigma, left_t, right_t):
+    '''
+    Defines a truncated normal of given mean, sigma, and truncation at left_t and right_t
+    '''
     return stats.norm.pdf(x, mean, sigma) / (stats.norm.cdf(right_t) - stats.norm.cdf(left_t))
 
 def importance_sampling_mc_vectorized(integrand_function, sampling_function, number_sampled_points):
+    '''
+    ATTENTION : specialized function
+    This code evaluates the integral in the proposed exercise through importance sampling with the function sampling_function
+    contained in an array. The code contains the sampling from this distribution through a rejection sampling using a truncated 
+    normal an parameter c. Moreover the code is vectorized for speed. 
+    '''
     left_bound  = 0
     right_bound = np.pi / 2
     mean = np.pi/2
@@ -38,4 +55,3 @@ def importance_sampling_mc_vectorized(integrand_function, sampling_function, num
     estimate = np.sum(func_values) / number_sampled_points                
     variance = np.sum(func_values**2) / number_sampled_points - estimate**2
     return estimate , np.sqrt(variance / number_sampled_points)
-
