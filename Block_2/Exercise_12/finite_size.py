3beda2bbc8089b437184f1e541ae39d153aa01dc
nunziatodamino <nunziatodamino@gmail.com>
2025-04-04 11:10:02 +0200

    fixed error in exercise 12

--- Block_2/Exercise_12/finite_size.py
+++ Block_2/Exercise_12/finite_size.py
@@ -9,8 +9,8 @@ y_values = []
 for num in numbers:
     filename = f'beta_{num}.npy'
     beta_value = np.load(filename)
-    x_values.append(beta_value)
-    y_values.append(num**(-0.5))
+    x_values.append(num**(-0.5))
+    y_values.append(beta_value)
 
 x_values = np.array(x_values)
 y_values = np.array(y_values)
@@ -23,10 +23,9 @@ best_fit_y = m * x_values + c
 plt.figure(figsize=(8, 6))
 plt.plot(x_values, y_values, 'o', label='Data Points', markersize=8)
 plt.plot(x_values, best_fit_y, '-', label=f'Best Fit Line: y = {m:.3f}x + {c:.3f}', color='r')
-plt.xlabel('Inverse temperature')
-plt.ylabel('Number of monomers^(-1/2)')
+plt.ylabel('Inverse temperature')
+plt.xlabel('Number of monomers^(-1/2)')
 plt.legend()
 plt.grid(True)
 plt.show()
-
 
